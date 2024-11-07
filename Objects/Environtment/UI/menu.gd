bc50a741a8310f3792cbc7402649c3bf0c2a36f2
Agung Satrio <augustineenr@gmail.com>
2024-11-07 22:37:55 +0700

    task finished minus bg

--- Objects/Environtment/UI/menu.gd
+++ Objects/Environtment/UI/menu.gd
@@ -1,48 +1,21 @@
 class_name Menu
 extends Control
+@onready var mode: Button = $"MarginContainer/HBoxContainer/VBC NEXT/Mode"as Button
 
-@onready var task: Button = $"MarginContainer/HBoxContainer/VBC Profile and Task/Task" as Button
-@onready var mode: Button = $"MarginContainer/HBoxContainer/VBC Mode/Mode" as Button
+@onready var task: Button = $"MarginContainer/HBoxContainer/VBC Profile and Task/HBoxContainer/Task" as Button
+
 @onready var setting: Button = $"MarginContainer/HBoxContainer/VBC Setting and Play/Setting" as Button
 @onready var play: Button = $"MarginContainer/HBoxContainer/VBC Setting and Play/Play" as Button
 @onready var settings: Settings = $Settings as Settings
 @onready var margin_container: MarginContainer = $MarginContainer as MarginContainer
-@onready var prev: Button = $"MarginContainer/HBoxContainer/VBC PREV/PREV" as Button
-@onready var next: Button = $"MarginContainer/HBoxContainer/VBC NEXT/NEXT" as Button
 @onready var avatar: Button = $"MarginContainer/HBoxContainer/VBC Profile and Task/HBC Profile/TextureRect2/Avatar" as Button
-@onready var profile: Control = $Profile
 
-
-
-
+@onready var customize: Custom = $Customize as Custom
+@onready var custom: Button = $"MarginContainer/HBoxContainer/VBC Profile and Task/HBoxContainer/Custom" as Button
 
 var charvalue = 0
 func _ready() :
 	handle_connecting_signal()
-	
-func update_chara(val) :
-	charvalue += val
-	charvalue = min(charvalue, 2)
-	charvalue = max(-1, charvalue)
-		
-	if charvalue > 1:
-		charvalue = 0
-		print(charvalue)
-		
-	elif charvalue < 0:
-		charvalue = 1
-		print(charvalue)
-	
-	else:
-		print(charvalue)
-		
-	$"MarginContainer/HBoxContainer/VBC Mode/Node2D/Sprite".frame = charvalue
-	
-func on_prev_pressed():
-	update_chara(-1)
-
-func on_next_pressed():
-	update_chara(+1)
 	
 func on_ava_pressed():
 	get_tree().change_scene_to_file("res://Objects/Environtment/UI/profile.tscn")
@@ -59,10 +32,21 @@ func on_exit_on_setting() -> void:
 	margin_container.visible = true
 	settings.visible = false
 	
+func on_custom_pressed() -> void:
+	margin_container.visible = false
+	customize.set_process(false)
+	customize.visible = true
+
+func on_confirm_on_custom() -> void:
+	margin_container.visible = true
+	customize.visible = false
+	
 func handle_connecting_signal() -> void:
 	setting.button_down.connect(on_setting_pressed)
 	settings.exit_setting.connect(on_exit_on_setting)
-	prev.button_down.connect(on_prev_pressed)
-	next.button_down.connect(on_next_pressed)
+	
 	avatar.button_down.connect(on_ava_pressed)
 	play.button_down.connect(on_play_pressed)
+	
+	custom.button_down.connect(on_custom_pressed)
+	customize.confirm_confirm.connect(on_confirm_on_custom)
--- Objects/Environtment/UI/profile.gd
+++ Objects/Environtment/UI/profile.gd
@@ -4,8 +4,6 @@ extends Control
 @onready var exit: Button = $MarginContainer/VBoxContainer/Exit as Button
 @onready var sign_out: Button = $"MarginContainer/VBoxContainer2/HBoxContainer/VBoxContainer3/Sign Out" as Button
 
-signal exit_ava
-
 func _ready() -> void:
 	sign_out.button_down.connect(on_signout_pressed)
 	exit.button_down.connect(on_exit_pressed)
@@ -15,5 +13,4 @@ func on_signout_pressed():
 	get_tree().change_scene_to_file("res://Objects/Environtment/UI/login.tscn")
 	
 func on_exit_pressed () -> void:
-	exit_ava.emit()
-	set_process(false)
+	get_tree().change_scene_to_file("res://Objects/Environtment/UI/menu.tscn")
