f0088fee6aaa351a5f1249380989e2f91b96ea97
Rymess007 <823mfatham@gmail.com>
2024-11-09 14:24:32 +0700

    Style dialog

--- UI Asset/Script/LockSystem.gd
+++ UI Asset/Script/LockSystem.gd
@@ -4,7 +4,7 @@ extends Node
 
 # Variabel untuk menyimpan level yang terbuka
 var unlocked_level: int = 0
-var total_levels: int = 5  # Misalkan total level ada 10
+var total_levels: int = 5  # Misalkan total level ada 5
 
 # Array untuk menyimpan referensi ke Button dan TextureRect
 var level_buttons: Array = []
@@ -12,8 +12,8 @@ var lock_icons: Array = []  # Array untuk menyimpan TextureRect
 
 func _ready():
 	# Mengisi array dengan referensi ke Button dan TextureRect
-	for i in range(total_levels):
-		var button = $".".get_child(i)  # Mengambil button dari node container
+	for i in range(get_child_count()):  # Use get_child_count() to avoid out of bounds
+		var button = get_child(i)  # Mengambil button dari node container
 		if button is Button:
 			level_buttons.append(button)
 			var lock_icon = button.get_node("LockIcon")  # Mengambil TextureRect dari button
@@ -32,17 +32,19 @@ func load_levels():
 		var json = JSON.new()
 		var parsed_data = json.parse(json_data)
 		
-		if parsed_data.error == OK:
-			unlocked_level = parsed_data.result.level
+		if parsed_data == OK:
+			unlocked_level = json.get_data().get("level", 0)
 			print("level yang terbuka", unlocked_level)
 			check_levels()
 		else:
-			print("error parsing JSON: ", parsed_data.error_string)
+			print("error parsing JSON")
 	else:
 		print("error opening file")
 
 func check_levels():
-	for i in range(total_levels):
+	# Ensure we do not go out of bounds
+	var max_levels = min(total_levels, level_buttons.size(), lock_icons.size())
+	for i in range(max_levels):
 		if i < unlocked_level:
 			level_buttons[i].disabled = false  # Aktifkan button
 			lock_icons[i].visible = false  # Sembunyikan TextureRect
